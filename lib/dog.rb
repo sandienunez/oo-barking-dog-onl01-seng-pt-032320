80dcbc7be5cb11ae702fadbf5eac5d17f4c7f596
Sandie Nunez <snunez@vt.edu>
2020-04-07 03:56:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Your code goes here!
+class Dog
+  def name=(name)
+    @name = name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
+
+
+#def initialize(name) 
+   # @name = Dog 
+#  end
+  
+#class Person
+ # def initialize(name)
+#    @name = name
+ # end
+#  def name                   # creates getter method for name
+#    @name
+ # end
+#end
+#mike = Person.new('Mike')
+#mike.name                 
